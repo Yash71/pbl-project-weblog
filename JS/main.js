4ad0c2a57ab7955ab8e35cb2c2127b3383573a8c
Gladingi <yashrajesh71@gmail.com>
2021-06-17 15:04:33 +0530

    Added scroll in animation Small basic touch-ups i.e. Comments and code styling

--- JS/main.js
+++ JS/main.js
@@ -19,29 +19,31 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
-});
-// typing effect
-var typed = new Typed("#typed", {
-    stringsElement: "#string-type",
-    typeSpeed: 50,
-    loop: true,
+    // typing effect
+    var typed = new Typed("#typed", {
+      stringsElement: "#string-type",
+      typeSpeed: 50,
+      loop: true,
+    });
+    
+    var typed = new Typed("#typed1", {
+      stringsElement: "#string-type1",
+      typeSpeed: 110,
+      loop: true,
+    });
+    //owl-carousel
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      responsive: responsive
+    });
+    // top scroller button
+    $(".tpscrl-btn span").click(function(){
+      $("html,body").animate({
+        scrollTop:0
+      },1000);
+    })
+    // scroll in animation
+    AOS.init();
   });
-  
-  var typed = new Typed("#typed1", {
-    stringsElement: "#string-type1",
-    typeSpeed: 110,
-    loop: true,
-  });
-  //owl-carousel
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    responsive: responsive
-  });
-  // top scroller button
-  $(".tpscrl-btn span").click(function(){
-    $("html,body").animate({
-      scrollTop:0
-    },1000);
-  })
